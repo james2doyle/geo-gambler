98e658ac676f2e372e7e6129f21719560c204dda
james2doyle <james2doyle@gmail.com>
2016-09-24 19:32:11 -0700

    update map markers for the new distance output

--- public/src/scripts/app/map-marker-click.js
+++ public/src/scripts/app/map-marker-click.js
@@ -3,7 +3,7 @@ const storage = require('../app/storage');
 function markerTemplate(location) {
   return `<div class="on-map-marker">
     <h4>${location.title}</h4>
-    <div><small>${location.distance}m away</small></div>
+    <div><small>${Math.round(location.distance)}m away</small></div>
     <div><small>${location.wallet} Credit Jackpot</small></div>
     <div><small>Expires in 22 mins</small></div>
     <div><button type="button" onclick="btnEvents.handlePlayClick(${location.id})">Play!</button></div>
@@ -16,9 +16,8 @@ module.exports = function (infowindow, location, map) {
   .then((response) => {
     return response.json();
   }).then((res) => {
-    console.log(res);
     map.panTo(this.getPosition());
-    infowindow.setContent(markerTemplate(location));
+    infowindow.setContent(markerTemplate(res));
     infowindow.open(map, this);
   }).catch((err) => {
     console.error(err);
